034e9c763289f85c2858a672040a08f3783868a7
megan023 <76418073+megan023@users.noreply.github.com>
2024-05-21 00:18:15 -0700

    Add files via upload
    badly (print statement) commented optimizer

---
+++ optimizer.py
@@ -0,0 +1,115 @@
+from typing import Callable, Iterable, Tuple
+import math
+import time
+
+import torch
+from torch.optim import Optimizer
+
+
+class AdamW(Optimizer):
+    def __init__(
+            self,
+            params: Iterable[torch.nn.parameter.Parameter],
+            lr: float = 1e-3,
+            betas: Tuple[float, float] = (0.9, 0.999),
+            eps: float = 1e-6,
+            weight_decay: float = 0.0,
+            correct_bias: bool = True,
+    ):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {} - should be >= 0.0".format(lr))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[1]))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
+        super().__init__(params, defaults)
+
+    def step(self, closure: Callable = None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
+
+                # State should be stored in this dictionary.
+                state = self.state[p]
+
+                # Access hyperparameters from the `group` dictionary.
+                alpha = group["lr"]
+
+                # Complete the implementation of AdamW here, reading and saving
+                # your state in the `state` dictionary above.
+                # The hyperparameters can be read from the `group` dictionary
+                # (they are lr, betas, eps, weight_decay, as saved in the constructor).
+                #
+                # To complete this implementation:
+                # 1. Update the first and second moments of the gradients.
+                # 2. Apply bias correction
+                #    (using the "efficient version" given in https://arxiv.org/abs/1412.6980;
+                #     also given in the pseudo-code in the project description).
+                # 3. Update parameters (p.data).
+                # 4. Apply weight decay after the main gradient-based updates.
+                # Refer to the default project handout for more details.
+
+                ### TODO
+                # print("state", self.state)
+                
+                if 't' in state:
+                    t = state['t'] + 1
+                else: 
+                    t = 1
+                state['t'] = t
+                # print("t", t)
+                if 'm' in state:
+                    m_l = state['m'] 
+                else: 
+                    m_l = torch.zeros(p.size())
+
+                if 'v' in state:
+                    v_l = state['v'] 
+                else:
+                    v_l = torch.zeros(p.size())
+                
+                beta_1 = group['betas'][0]
+                beta_2 = group['betas'][1]
+
+                # print("beta 1 ", beta_1)
+                # print("beta 2 ", beta_2)
+
+                # print('m_l', m_l)
+                # print('v_l', v_l)
+                
+                state['m'] = beta_1*m_l + (1-beta_1)*grad
+                state['v'] = beta_2*v_l + (1-beta_2)*(grad*grad)
+                # print('m', state['m'])
+                # print('v', state['v'])
+
+                # print("b^t", pow(beta_1,t))
+                m_hat = torch.div(state['m'],(torch.ones(p.size())-pow(beta_1,t)))
+                v_hat = state['v']/(torch.ones(p.size())-beta_2**t)
+
+                # print('m_hat',m_hat)
+                # print('v_hat', v_hat)
+                p.data = p.data - (alpha*m_hat/(torch.sqrt(v_hat) + group['eps']))
+
+                #weight decay
+                # print("weight decay", group['weight_decay'])
+                p.data = p.data - alpha*group['weight_decay']*p.data
+                # print("p", p)
+            # print("group", group)
+            
+
+                #raise NotImplementedError
+                # time.sleep(1000)
+            #p.data = above calculations 
+
+        return loss
